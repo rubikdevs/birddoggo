2ceeddb152c1e947419be6b64a193fd8b3983727
niconistal <nicnistal@Nicolass-MacBook-Pro-2.local>
2013-11-01 23:45:25 -0300

    asdasdaSd@

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,29 @@
-
+function copy_inputs(from,to){
+	from.find('input[type=text]').each(function(index){
+		to.find('input[type=text]').eq(index).val($(this).val());
+	});
+}
 (function() {
     var updatePosition = function() {
         var top = document.body.scrollTop||document.documentElement.scrollTop,
             style = {};
         if (top > 245 ) {
             style.top = 0;
-            $('.searcharea').css('opacity','0');
+            
         } else {
             style.top = '-70px';
-            $('.searcharea').css('opacity','1');
         }
         $('header').css(style);
     }
     var throttled = _.throttle(updatePosition, 100);
     $(window).scroll(throttled);
-
+    var $bodyfields = $('.bodyfields');
+    var $headerfields = $('.headerfields');
+	$bodyfields.on('keyup', 'input[type=text]',function(){
+		copy_inputs($bodyfields,$headerfields);
+	});
+	$headerfields.on('keyup', 'input[type=text]', function(){
+		copy_inputs($headerfields,$bodyfields);
+	});
 }(window.birddoggo));
+
